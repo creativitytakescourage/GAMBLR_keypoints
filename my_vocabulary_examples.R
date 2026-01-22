67971d31d6c49df0301c841fcdc717019b92ad2e
Francesca <2231275@learn.vsb.bc.ca>
2026-01-21 21:26:26 -0800

    added my vocabulary examples to new branch

---
+++ my_vocabulary_examples.R
@@ -0,0 +1,48 @@
+# My vocabulary examples
+
+mysum <- function(a, b){
+  message("will calculate the sum of a and b")
+  outcome <- sum(a, b)
+  print(outcome)
+  message("100%")
+}
+mysum(2, 5)
+mysum(1120, 3956)
+mysum(2, "x")
+mysum(4, 5)
+
+1 == "x"
+
+"x" <- 1
+
+myaverage <- function(a, b){
+  result <- (a + b)/2
+  return(result)
+}
+
+results <- myaverage(10, 12)
+print(results)
+
+!0
+
+!5 %in% c(1, 3, 5)
+
+my_grocery <- c("apples", "olive oil")
+my_grocery
+
+my_snacks <- c(1, "cookies", 2)
+
+mycombined_shopping <- c(my_grocery, my_snacks)
+mycombined_shopping
+
+"x"
+x
+x <- 100
+x
+x <- 100 + 1
+x
+x <- "hello"
+x
+x <- results
+x <- print 
+x(results)
